1a224898506b302bc5553d226f74bc6d4b1abe61
Robdex Smoke <smoke@example.invalid>
2026-06-27 16:01:09 -0700

    big moves

---
+++ frontend/robdex/robdexTests/robdexTests.swift
@@ -0,0 +1,16 @@
+//
+//  robdexTests.swift
+//  robdexTests
+//
+//  Created by Robert Sale on 6/27/26.
+//
+
+import Testing
+
+struct robdexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
